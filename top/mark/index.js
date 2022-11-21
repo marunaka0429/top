bddd087c3e232ed682d22e8399ddef05c05c1894
nakajima <103499972+marunaka0429@users.noreply.github.com>
2022-11-22 00:36:29 +0900

    Add files via upload

---
+++ top/mark/index.js
@@ -0,0 +1,9 @@
+$('.menu').slick({
+    autoplay: false,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 2,
+    slidesToScroll: 2,//1回のスクロールで3枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,
+});
